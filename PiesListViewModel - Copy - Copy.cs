768ac18551655222fe0ccfacf8ebbfad35b90682
Rpbts <51463854+Rpbts@users.noreply.github.com>
2019-11-26 15:25:14 -0500

    Add files via upload

---
+++ PiesListViewModel - Copy - Copy.cs
@@ -0,0 +1,15 @@
+﻿using BethaniesPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethaniesPieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+
+}
